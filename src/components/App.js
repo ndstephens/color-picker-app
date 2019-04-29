1c57ec0f4de7e9d79a68abf34663bd8f2e537fc3
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-29 11:38:41 -0400

    display palette from route url

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,25 @@ import { generatePalette } from '../colorHelpers'
 import Palette from './Palette'
 
 class App extends Component {
+  findPalette = id => seedColors.find(palette => palette.id === id)
+
   render() {
-    console.log(generatePalette(seedColors[4]))
+    // console.log(generatePalette(seedColors[4]))
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>Palette</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     )
   }
 }
