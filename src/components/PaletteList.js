7295f7ae0c949afc0cc56a1470922b84f6083e9f
Nathaniel Stephens <ndstephens@gmail.com>
2019-04-29 12:47:18 -0400

    styling of PaletteList

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/styles'
 
 import MiniPalette from './MiniPalette'
 
+const styles = {
+  root: {
+    backgroundColor: 'blue',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    color: 'white',
+  },
+  palettes: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%',
+  },
+}
+
 class PaletteList extends Component {
   render() {
-    const { palettes } = this.props
+    const { classes, palettes } = this.props
 
     const paletteList = palettes.map(palette => <MiniPalette {...palette} />)
 
     return (
-      <div className="PaletteList">
-        <h1>React Colors</h1>
-        {paletteList}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+          </nav>
+          <div className={classes.palettes}>{paletteList}</div>
+        </div>
       </div>
     )
   }
 }
 
-export default PaletteList
+export default withStyles(styles)(PaletteList)
