68b67201d51ee42dfaa385174a7159eed0d0b10b
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-05 17:53:02 -0400

    make Colorbox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js'
+import mq from './mediaQueries'
 
 export default {
   ColorBox: {
@@ -14,6 +15,7 @@ export default {
         ? 'rgba(0, 0, 0, 0.65)'
         : 'white',
   },
+
   colorName: {
     width: '100%',
     position: 'absolute',
@@ -23,6 +25,18 @@ export default {
     letterSpacing: '1px',
     textTransform: 'uppercase',
     fontSize: '12px',
+  },
+  moreBtn: {
+    position: 'absolute',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    border: 'none',
+    width: '60px',
+    height: '30px',
+    textAlign: 'center',
+    lineHeight: '30px',
+    textTransform: 'uppercase',
   },
   copyBtn: {
     position: 'absolute',
@@ -43,33 +57,6 @@ export default {
     cursor: 'pointer',
     opacity: '0',
     transition: 'opacity 500ms',
-  },
-  moreBtn: {
-    position: 'absolute',
-    right: '0',
-    bottom: '0',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    border: 'none',
-    width: '60px',
-    height: '30px',
-    textAlign: 'center',
-    lineHeight: '30px',
-    textTransform: 'uppercase',
-  },
-
-  copyMsgHeading: {
-    fontWeight: '400',
-    textShadow: '1px 2px #000',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    width: '100%',
-    textAlign: 'center',
-    marginBottom: '0',
-    padding: '1rem',
-    textTransform: 'uppercase',
-  },
-  copyMsgText: {
-    fontSize: '2rem',
-    fontWeight: '100',
   },
 
   copyOverlay: {
@@ -97,15 +84,36 @@ export default {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    fontSize: '4rem',
     transform: 'scale(0)',
     opacity: '0',
     color: 'white',
+    fontSize: '4rem',
+    [mq.down('md')]: {
+      fontSize: '3rem',
+    },
+    [mq.down('xs')]: {
+      fontSize: '2rem',
+    },
   },
   displayMsg: {
     opacity: '1',
     transform: 'scale(1)',
     zIndex: '20',
     transition: 'all 250ms ease-in 200ms',
+  },
+
+  copyMsgHeading: {
+    fontWeight: '400',
+    textShadow: '1px 2px #000',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: '0',
+    padding: '1rem',
+    textTransform: 'uppercase',
+  },
+  copyMsgText: {
+    fontSize: '2rem',
+    fontWeight: '100',
   },
 }
