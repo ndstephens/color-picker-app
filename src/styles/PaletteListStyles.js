89386136dc4a59c0a815aa4e1c77f1b952c8eaea
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-06 10:54:40 -0400

    add svg background

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,16 @@
+import bgSVG from './background.svg'
+
 export default {
   root: {
-    backgroundColor: 'blue',
-    minHeight: '100vh',
+    // minHeight: '100vh',
+    height: '100vh',
+    overflow: 'scroll',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
     paddingBottom: '2rem',
+    backgroundColor: '#cccccc',
+    backgroundImage: `url(${bgSVG})`,
   },
   container: {
     width: '84%',
@@ -17,12 +22,17 @@ export default {
   },
   nav: {
     display: 'flex',
+    flexWrap: 'wrap',
     width: '100%',
     justifyContent: 'space-between',
     alignItems: 'center',
-    color: 'white',
+    color: '#1e8feb',
+    '& h1': {
+      fontSize: '2.5rem',
+    },
     '& a': {
       color: 'currentColor',
+      fontSize: '1.5rem',
     },
   },
   palettes: {
