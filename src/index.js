0a9e257ffd344594edfa79ed897f40073fa1df42
Nathaniel Stephens <ndstephens@gmail.com>
2019-06-08 12:38:29 -0400

    hopeful fix for production CSS classname issue

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
+import JssProvider from 'react-jss/lib/JssProvider'
+import { createGenerateClassName } from '@material-ui/core/styles'
 import App from './components/App'
 
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: false,
+  productionPrefix: 'c',
+})
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <JssProvider generateClassName={generateClassName}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </JssProvider>,
   document.getElementById('root')
 )
 
